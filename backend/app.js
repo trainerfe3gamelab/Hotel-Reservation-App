1dea550915afe1ac1c6c6e24bd2eac37e351f2ce
unknown <teukufaandii>
2024-06-05 19:32:05 +0700

    connect db

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const dotenv = require('dotenv');
-const auth = require('./backend/middleware/auth');
+const mysql = require('mysql');
 
 // Initialize dotenv to use .env variables
 dotenv.config();
 
 const app = express();
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+app.get("/users", (req, res) => {
+    db.query("SELECT * FROM users", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
 
 // Middleware to parse cookies
 app.use(cookieParser());
 
-// Example route to demonstrate verifyToken middleware
-app.get('/protected', auth, (req, res) => {
-    res.json({ message: "This is a protected route", userId: req.userId });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 // Start server
